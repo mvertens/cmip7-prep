f99ea6bc08afd730cbe03dbc38ee4fb6686d8927
Mariana Vertenstein <marianav@met.no>
2026-02-27 18:19:25 +0100

    fixed pathname for TABLES_noresm

--- scripts/cmor_driver.py
+++ scripts/cmor_driver.py
@@ -54,7 +54,7 @@ _DATE_RE = re.compile(
 
 # Path for cmor tables
 TABLES_cesm = "/glade/derecho/scratch/jedwards/cmip7-prep/cmip7-cmor-tables/"
-TABLES_noresm = "/nird/home/mvertens/packages/cmip7-prep/cmip7-cmor-tables/"
+TABLES_noresm = "/nird/datalake/NS9560K/mvertens/packages/cmip7-prep/cmip7-cmor-tables/"
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
